9501dd6ee0c4b2ae1e7c0bdb57a796a90100ae0f
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-06-15 09:36:37 +0200

    Firebase setup

--- legacy-app/src/environments/environment.prod.ts
+++ legacy-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDiDVg6ggmSY-Z8Iu5dWO83Mg4GTmt8Zl0',
+    authDomain: 'esn-tumi.firebaseapp.com',
+    databaseURL: 'https://esn-tumi.firebaseio.com',
+    projectId: 'esn-tumi',
+    storageBucket: '',
+    messagingSenderId: '756904945827',
+    appId: '1:756904945827:web:c4b8570bf907f627'
+  }
 };
--- legacy-app/src/environments/environment.ts
+++ legacy-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDiDVg6ggmSY-Z8Iu5dWO83Mg4GTmt8Zl0',
+    authDomain: 'esn-tumi.firebaseapp.com',
+    databaseURL: 'https://esn-tumi.firebaseio.com',
+    projectId: 'esn-tumi',
+    storageBucket: '',
+    messagingSenderId: '756904945827',
+    appId: '1:756904945827:web:c4b8570bf907f627'
+  }
 };
 
 /*
