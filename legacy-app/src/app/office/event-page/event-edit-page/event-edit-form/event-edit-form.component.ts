d1322f649a638186c523b05b5e6b1fe355b246b1
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-06-23 09:06:48 +0200

    Fix event edit form time handling

--- legacy-app/src/app/office/event-page/event-edit-page/event-edit-form/event-edit-form.component.ts
+++ legacy-app/src/app/office/event-page/event-edit-page/event-edit-form/event-edit-form.component.ts
@@ -4,13 +4,16 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges
 } from '@angular/core';
 import { TumiEvent } from '../../../../shared/services/event.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-event-edit-form',
@@ -18,19 +21,44 @@ import * as moment from 'moment';
   styleUrls: ['./event-edit-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EventEditFormComponent implements OnInit, OnChanges {
+export class EventEditFormComponent implements OnInit, OnChanges, OnDestroy {
   @Input() event: TumiEvent;
   @Output() save = new EventEmitter<TumiEvent>();
+  destroyed$ = new Subject();
   eventForm: FormGroup;
+  iconUrl$: Observable<string>;
 
   constructor(private fb: FormBuilder) {
     this.eventForm = this.fb.group({
-      name: '',
-      start: moment(),
-      end: moment(),
-      description: '',
-      participantSpots: 0,
-      tutorSpots: 0
+      name: ['', Validators.required],
+      start: [null, Validators.required],
+      startTime: ['', Validators.pattern('^([01]\\d|2[0-3]):([0-5]\\d)$')],
+      end: [null, Validators.required],
+      endTime: ['', Validators.pattern('^([01]\\d|2[0-3]):([0-5]\\d)$')],
+      description: ['', Validators.required],
+      participantSpots: [0, Validators.required],
+      tutorSpots: [0, Validators.required],
+      hasOnlineSignup: [false, Validators.required],
+      hasFee: [false, Validators.required],
+      price: { value: 0, disabled: true },
+      icon: ['']
+    });
+    this.iconUrl$ = this.eventForm.get('icon').valueChanges.pipe(
+      debounceTime(200),
+      map(icon => `https://png.icons8.com/color/${icon}/62`)
+    );
+    this.eventForm.get('hasOnlineSignup').valueChanges.subscribe(value => {
+      if (value) {
+        this.eventForm.get('hasFee').setValue(false);
+      }
+    });
+    this.eventForm.get('hasFee').valueChanges.subscribe(value => {
+      if (value) {
+        this.eventForm.get('hasOnlineSignup').setValue(false);
+        this.eventForm.get('price').enable();
+      } else {
+        this.eventForm.get('price').disable();
+      }
     });
   }
 
@@ -38,12 +66,42 @@ export class EventEditFormComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.event && !changes.event.firstChange) {
-      this.eventForm.reset(changes.event.currentValue);
+      const event = changes.event.currentValue;
+      const startTime = event.start.format('HH:mm');
+      const endTime = event.end.format('HH:mm');
+      event.start.startOf('day');
+      event.end.startOf('day');
+      this.eventForm.reset({
+        ...event,
+        startTime,
+        endTime
+      });
     }
   }
 
   saveChanges() {
+    const value = this.eventForm.value;
     this.eventForm.disable();
-    this.save.emit({ ...this.event, ...this.eventForm.value });
+    const startTime = value.startTime.split(':');
+    const endTime = value.endTime.split(':');
+    value.start.hours(startTime[0]).minutes(startTime[1]);
+    value.end.hours(endTime[0]).minutes(endTime[1]);
+    const formData = {
+      name: value.name,
+      description: value.description,
+      start: value.start,
+      end: value.end,
+      participantSpots: value.participantSpots,
+      tutorSpots: value.tutorSpots,
+      hasOnlineSignup: value.hasOnlineSignup,
+      hasFee: value.hasFee,
+      price: value.price || 0,
+      icon: value.icon
+    };
+    this.save.emit({ ...this.event, ...formData });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.complete();
   }
 }
