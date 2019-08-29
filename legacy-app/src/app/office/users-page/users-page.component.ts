d7f12f2be889bdace80a1fa43f952281bd694f52
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-08-29 14:18:50 +0200

    Update the office component

---
+++ legacy-app/src/app/office/users-page/users-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-page',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss']
+})
+export class UsersPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
