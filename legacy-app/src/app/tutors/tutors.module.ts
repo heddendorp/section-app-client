a2ef54c7de3e9955bfe33b944bc1c54ad5270abd
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-10-09 13:48:10 +0200

    Fix long links in events

--- legacy-app/src/app/tutors/tutors.module.ts
+++ legacy-app/src/app/tutors/tutors.module.ts
@@ -1,6 +1,25 @@
+/*
+ *     The TUMi app provides a modern way of managing events for an esn section.
+ *     Copyright (C) 2019  Lukas Heddendorp
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 import { SharedModule } from '../shared/shared.module';
 import { LoadFullEventResolver } from './resolvers/load-full-event.resolver';
 import { DisplayEventInfoComponent } from './run-events-page/manage-event/display-event-info/display-event-info.component';
@@ -25,6 +44,6 @@ const routes: Routes = [
     DisplayEventUsersComponent
   ],
   providers: [LoadFullEventResolver],
-  imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
+  imports: [CommonModule, RouterModule.forChild(routes), MarkdownModule.forChild(), SharedModule]
 })
 export class TutorsModule {}
