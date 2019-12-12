349a593fcc15da50dcf1d9d902ac320ae2bee2e0
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-12-12 10:02:19 +0100

    Update NA page design

---
+++ legacy-app/src/app/pages/na-page/na-page.component.spec.ts
@@ -0,0 +1,42 @@
+/*
+ *     The TUMi app provides a modern way of managing events for an esn section.
+ *     Copyright (C) 2019  Lukas Heddendorp
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NaPageComponent } from './na-page.component';
+
+describe('NaPageComponent', () => {
+  let component: NaPageComponent;
+  let fixture: ComponentFixture<NaPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NaPageComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NaPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
