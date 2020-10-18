83c6549d1bbda212897262e667a91539a3e67e43
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2020-10-18 19:00:15 +0200

    fix(events): Display tutors already on the event

--- legacy-app/src/app/modules/events/components/event-participants/event-participants.component.ts
+++ legacy-app/src/app/modules/events/components/event-participants/event-participants.component.ts
@@ -1,11 +1,8 @@
-import {
-  Component,
-  OnInit,
-  ChangeDetectionStrategy,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { EventService } from '../../../../services/event.service';
-import { first } from 'rxjs/operators';
+import { AuthService } from '../../../../services/auth.service';
+import { Observable } from 'rxjs';
+import { MoneyService } from '../../../../services/money.service';
 
 @Component({
   selector: 'app-event-participants',
@@ -13,11 +10,17 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./event-participants.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventParticipantsComponent implements OnInit {
+export class EventParticipantsComponent {
   @Input() event: any;
-  constructor(private eventService: EventService) {}
+  $isAdmin: Observable<boolean>;
 
-  ngOnInit(): void {}
+  constructor(
+    private eventService: EventService,
+    private moneyService: MoneyService,
+    auth: AuthService
+  ) {
+    this.$isAdmin = auth.isAdmin$;
+  }
 
   async toggleRegistration(registration: any): Promise<void> {
     await this.eventService.updateRegistration(this.event.id, registration.id, {
@@ -25,11 +28,28 @@ export class EventParticipantsComponent implements OnInit {
     });
   }
 
-  public async removeRegitstration(id: string): Promise<void> {
-    await this.eventService.removeRegistration(this.event.id, id);
+  public async removeRegistration(
+    registration: any,
+    refund = false
+  ): Promise<void> {
+    if (this.event.hasFee && refund) {
+      await this.moneyService.addEventTransaction(
+        `Event Refund (${this.event.name}) payed to ${registration.user.firstName} ${registration.user.lastName} (${registration.user.email})`,
+        this.event,
+        registration.user,
+        'refund'
+      );
+    }
+    await this.eventService.removeRegistration(this.event.id, registration.id);
   }
 
   stopPropagation($event: MouseEvent): void {
     $event.stopImmediatePropagation();
+  }
+
+  async removeTutor(id: string): Promise<void> {
+    await this.eventService.updateEvent(this.event.id, {
+      tutorSignups: this.event.tutorSignups.filter((s: string) => s !== id),
+    });
   }
 }
