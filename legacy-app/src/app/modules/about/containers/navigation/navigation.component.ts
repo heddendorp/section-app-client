0cc34f7052312fd80fd9ccaebab7a8f58b46fd2b
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-02-17 17:16:48 +0100

    fix(about): Update site metadata

--- legacy-app/src/app/modules/about/containers/navigation/navigation.component.ts
+++ legacy-app/src/app/modules/about/containers/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +8,33 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavigationComponent implements OnInit {
-  constructor() {}
+  constructor(private meta: Meta, private title: Title) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.title.setTitle('About TUMi');
+    this.meta.updateTag(
+      { property: 'og:title', content: 'About TUMi' },
+      "property='og:title'"
+    );
+    this.meta.updateTag(
+      { property: 'og:url', content: `https://tumi.esn.world/about` },
+      "property='og:url'"
+    );
+    this.meta.updateTag(
+      {
+        property: 'og:description',
+        content:
+          'Learn about ESN TUMi and how we can improve your student life',
+      },
+      "property='og:description'"
+    );
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content:
+          'Learn about ESN TUMi and how we can improve your student life',
+      },
+      "name='description'"
+    );
+  }
 }
