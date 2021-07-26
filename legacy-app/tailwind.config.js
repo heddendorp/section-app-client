1891991d1c833cb43e763128b900569616fa9be8
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-07-26 18:49:26 +0200

    add tailwind

---
+++ legacy-app/tailwind.config.js
@@ -0,0 +1,26 @@
+const { guessProductionMode } = require('@ngneat/tailwind');
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './apps/**/*.{html,ts,css,scss,sass,less,styl}',
+      './libs/**/*.{html,ts,css,scss,sass,less,styl}',
+    ],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
+};
