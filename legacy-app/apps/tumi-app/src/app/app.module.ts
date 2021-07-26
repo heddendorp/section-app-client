723490dd879ee2549fd325d59fdb74a435363e29
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-07-26 17:55:18 +0200

    add angular material

--- legacy-app/apps/tumi-app/src/app/app.module.ts
+++ legacy-app/apps/tumi-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
