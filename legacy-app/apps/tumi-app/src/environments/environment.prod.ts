04b82a9f316972aa27eb7bc13705abb430e93d8d
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-07-30 11:21:32 +0200

    add firebase info

--- legacy-app/apps/tumi-app/src/environments/environment.prod.ts
+++ legacy-app/apps/tumi-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyDiDVg6ggmSY-Z8Iu5dWO83Mg4GTmt8Zl0",
+    authDomain: "esn-tumi.firebaseapp.com",
+    databaseURL: "https://esn-tumi.firebaseio.com",
+    projectId: "esn-tumi",
+    storageBucket: "esn-tumi.appspot.com",
+    messagingSenderId: "756904945827",
+    appId: "1:756904945827:web:c4b8570bf907f627",
+    measurementId: "G-HN3R0ZJRDD"
+  }
 };
--- legacy-app/apps/tumi-app/src/environments/environment.ts
+++ legacy-app/apps/tumi-app/src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyDiDVg6ggmSY-Z8Iu5dWO83Mg4GTmt8Zl0",
+    authDomain: "esn-tumi.firebaseapp.com",
+    databaseURL: "https://esn-tumi.firebaseio.com",
+    projectId: "esn-tumi",
+    storageBucket: "esn-tumi.appspot.com",
+    messagingSenderId: "756904945827",
+    appId: "1:756904945827:web:c4b8570bf907f627",
+    measurementId: "G-HN3R0ZJRDD"
+  }
 };
 
 /*
