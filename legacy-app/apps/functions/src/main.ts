f04321e5e58662e80e467c07d3743f9e43b66497
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-09 14:05:15 +0200

    import tslib in function

--- legacy-app/apps/functions/src/main.ts
+++ legacy-app/apps/functions/src/main.ts
@@ -1,6 +1,7 @@
 import { fromBuffer } from 'pdf2pic';
 import { Buffer } from 'buffer';
 import { ToBase64Response } from 'pdf2pic/dist/types/toBase64Response';
+import 'tslib';
 
 module.exports = async (context, inputBlob) => {
   const response = (await fromBuffer(inputBlob)(1, true)) as ToBase64Response;
