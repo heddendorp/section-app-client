21c9e75fa721f0a210e3df6a7825dde381bcb4d5
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-10-09 14:48:29 +0200

    correctly send back buffer

--- legacy-app/apps/functions/src/main.ts
+++ legacy-app/apps/functions/src/main.ts
@@ -6,6 +6,22 @@ import 'tslib';
 export const run = async (context, inputBlob) => {
   const response = (await fromBuffer(inputBlob)(1, true)) as ToBase64Response;
   context.log(response);
-  context.bindings.imageBlob = Buffer.from(response.base64, 'base64');
   // const prisma = new PrismaClient();
+  const imageBuffer = decodeBase64Image(Buffer.from(response.base64, 'base64'));
+  context.imageBlob = imageBuffer.data;
+  context.done();
 };
+
+function decodeBase64Image(dataString) {
+  const matches = dataString.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
+    response = { type: undefined, data: undefined };
+
+  if (matches.length !== 3) {
+    throw new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
