c29f83aca991b3829a27860948b31de5b5bd2174
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-11-04 23:05:12 +0100

    fix: remove old data structure

--- legacy-app/apps/api/src/app/seeding.ts
+++ legacy-app/apps/api/src/app/seeding.ts
@@ -1,12 +1,10 @@
 import {
-  MembershipStatus, Prisma,
+  MembershipStatus,
+  Prisma,
   PrismaClient,
-  RegistrationStatus,
-  Role
+  Role,
 } from '@tumi/server-models';
 import * as Stripe from 'stripe';
-import { randomUUID } from 'crypto';
-import InputJsonArray = Prisma.InputJsonArray;
 
 const stripe = new Stripe.default.Stripe(process.env.STRIPE_KEY, {
   apiVersion: '2020-08-27',
@@ -49,8 +47,8 @@ export async function seedDB(prisma: PrismaClient) {
       },
     },
   });
-  await migrateEvents(prisma);
-  await migratePayments(prisma);
+  // await migrateEvents(prisma);
+  // await migratePayments(prisma);
   // const events = await prisma.tumiEvent.findMany();
   // const ages = await Promise.all(
   //   events.map((event) =>
@@ -94,93 +92,93 @@ export async function seedDB(prisma: PrismaClient) {
   // await transferEvents(prisma, tenant);
 }
 
-async function migratePayments(prisma: PrismaClient) {
-  const registrations = await prisma.eventRegistration.findMany({
-    where: { chargeId: { not: null } },
-  });
-  for (const registration of registrations) {
-    console.log('processing registration');
-    await prisma.eventRegistration.update({
-      where: { id: registration.id },
-      data: {
-        status:
-          registration.paymentStatus === 'succeeded'
-            ? RegistrationStatus.SUCCESSFUL
-            : RegistrationStatus.PENDING,
-      },
-    });
-    if (registration.paymentId) continue;
-    console.log('processing stripe payment');
-    const charge = await stripe.charges.retrieve(registration.chargeId, {
-      expand: ['balance_transaction'],
-    });
-    await prisma.eventRegistration.update({
-      where: { id: registration.id },
-      data: {
-        payment: {
-          create: {
-            status: charge.status,
-            paymentIntent:
-              typeof charge.payment_intent === 'string'
-                ? charge.payment_intent
-                : charge.payment_intent.id,
-            amount: charge.amount,
-            paymentMethod: charge.payment_method,
-            paymentMethodType: charge.payment_method_details.type,
-            user: { connect: { id: registration.userId } },
-            events: [],
-            checkoutSession: randomUUID(),
-            feeAmount:
-              typeof charge.balance_transaction === 'object'
-                ? charge.balance_transaction?.fee ?? null
-                : null,
-            netAmount:
-              typeof charge.balance_transaction === 'object'
-                ? charge.balance_transaction?.net ?? null
-                : null,
-          },
-        },
-      },
-    });
-  }
-}
+// async function migratePayments(prisma: PrismaClient) {
+//   const registrations = await prisma.eventRegistration.findMany({
+//     where: { chargeId: { not: null } },
+//   });
+//   for (const registration of registrations) {
+//     console.log('processing registration');
+//     await prisma.eventRegistration.update({
+//       where: { id: registration.id },
+//       data: {
+//         status:
+//           registration.paymentStatus === 'succeeded'
+//             ? RegistrationStatus.SUCCESSFUL
+//             : RegistrationStatus.PENDING,
+//       },
+//     });
+//     if (registration.paymentId) continue;
+//     console.log('processing stripe payment');
+//     const charge = await stripe.charges.retrieve(registration.chargeId, {
+//       expand: ['balance_transaction'],
+//     });
+//     await prisma.eventRegistration.update({
+//       where: { id: registration.id },
+//       data: {
+//         payment: {
+//           create: {
+//             status: charge.status,
+//             paymentIntent:
+//               typeof charge.payment_intent === 'string'
+//                 ? charge.payment_intent
+//                 : charge.payment_intent.id,
+//             amount: charge.amount,
+//             paymentMethod: charge.payment_method,
+//             paymentMethodType: charge.payment_method_details.type,
+//             user: { connect: { id: registration.userId } },
+//             events: [],
+//             checkoutSession: randomUUID(),
+//             feeAmount:
+//               typeof charge.balance_transaction === 'object'
+//                 ? charge.balance_transaction?.fee ?? null
+//                 : null,
+//             netAmount:
+//               typeof charge.balance_transaction === 'object'
+//                 ? charge.balance_transaction?.net ?? null
+//                 : null,
+//           },
+//         },
+//       },
+//     });
+//   }
+// }
 
-async function migrateEvents(prisma: PrismaClient) {
-  const eventsWithPrice = await prisma.tumiEvent.findMany({
-    where: { price: { not: null } },
-  });
-  for (const event of eventsWithPrice) {
-    const defaultPrice = {
-      defaultPrice: true,
-      allowedStatusList: [
-        MembershipStatus.NONE,
-        MembershipStatus.TRIAL,
-        MembershipStatus.FULL,
-        MembershipStatus.SPONSOR,
-        MembershipStatus.ALUMNI,
-      ],
-      esnCardRequired: false,
-      amount: event.price.toNumber(),
-    };
-    const prices: InputJsonArray = [];
-    prices.push(defaultPrice);
-    if (event.esnDiscount) {
-      prices.push({
-        defaultPrice: false,
-        allowedStatusList: [
-          MembershipStatus.NONE,
-          MembershipStatus.TRIAL,
-          MembershipStatus.FULL,
-          MembershipStatus.SPONSOR,
-          MembershipStatus.ALUMNI,
-        ],
-        esnCardRequired: true,
-        amount: event.discountedPrice.toNumber(),
-      });
-    }
-    await prisma.tumiEvent.update({
-      where: { id: event.id },
-      data: { prices: { options: prices } },
-    });
-  }
-}
+// async function migrateEvents(prisma: PrismaClient) {
+//   const eventsWithPrice = await prisma.tumiEvent.findMany({
+//     where: { price: { not: null } },
+//   });
+//   for (const event of eventsWithPrice) {
+//     const defaultPrice = {
+//       defaultPrice: true,
+//       allowedStatusList: [
+//         MembershipStatus.NONE,
+//         MembershipStatus.TRIAL,
+//         MembershipStatus.FULL,
+//         MembershipStatus.SPONSOR,
+//         MembershipStatus.ALUMNI,
+//       ],
+//       esnCardRequired: false,
+//       amount: event.price.toNumber(),
+//     };
+//     const prices: InputJsonArray = [];
+//     prices.push(defaultPrice);
+//     if (event.esnDiscount) {
+//       prices.push({
+//         defaultPrice: false,
+//         allowedStatusList: [
+//           MembershipStatus.NONE,
+//           MembershipStatus.TRIAL,
+//           MembershipStatus.FULL,
+//           MembershipStatus.SPONSOR,
+//           MembershipStatus.ALUMNI,
+//         ],
+//         esnCardRequired: true,
+//         amount: event.discountedPrice.toNumber(),
+//       });
+//     }
+//     await prisma.tumiEvent.update({
+//       where: { id: event.id },
+//       data: { prices: { options: prices } },
+//     });
+//   }
+// }
