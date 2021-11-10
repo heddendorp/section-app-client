2f2ff391971566f235464260b02a82170097c483
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-11-10 16:10:18 +0100

    feat: update style towards material you

--- legacy-app/tailwind.config.js
+++ legacy-app/tailwind.config.js
@@ -13,7 +13,47 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: '#0053db',
+          on: '#ffffff',
+          container: '#dae1ff',
+          onContainer: '#00164d',
+        },
+        secondary: {
+          default: '#585e71',
+          on: '#ffffff',
+          container: '#dde1f9',
+          onContainer: '#161b2c',
+        },
+        tertiary: {
+          default: '#745471',
+          on: '#ffffff',
+          container: '#ffd7f8',
+          onContainer: '#2b122b',
+        },
+        error: {
+          default: '#ba1b1b',
+          on: '#ffffff',
+          container: '#ffdad4',
+          onContainer: '#410001',
+        },
+        background: {
+          default: '#fefbff',
+          on: '#1b1b1f',
+        },
+        surface: {
+          default: '#fefbff',
+          on: '#1b1b1f',
+        },
+        variant: {
+          default: '#e1e1ec',
+          on: '#44464e',
+        },
+        outline: { default: '#75767f' },
+      },
+    },
   },
   variants: {
     extend: {},
