4a38d0af0f2b1d23f13d5fd9c6f5527de7d7b6a9
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-03-02 10:44:30 +0100

    feat: add party animals deployment

--- party-animals/app/root.tsx
+++ party-animals/app/root.tsx
@@ -4,13 +4,18 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './styles/app.css';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'New Remix App' };
 };
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export default function App() {
   return (
