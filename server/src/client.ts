259c7bae766196ffabe622cab0e3ed14556279c2
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-03-25 13:07:33 +0100

    feat(legacy-app): update ad tile

--- server/src/client.ts
+++ server/src/client.ts
@@ -1,4 +1,34 @@
 import { PrismaClient } from './generated/prisma';
+import { getCurrentHub } from '@sentry/node';
 
 const prisma = new PrismaClient();
+prisma.$use(async (params, next) => {
+  const { model, action, runInTransaction, args } = params;
+  const description = [model, action].filter(Boolean).join('.');
+  const data = {
+    model,
+    action,
+    runInTransaction,
+    args,
+  };
+  const scope = getCurrentHub().getScope();
+  const parentSpan = scope?.getSpan();
+  const span = parentSpan?.startChild({
+    op: 'db',
+    description,
+    data,
+  });
+
+  // optional but nice
+  scope?.addBreadcrumb({
+    category: 'db',
+    message: description,
+    data,
+  });
+
+  const result = await next(params);
+  span?.finish();
+
+  return result;
+});
 export default prisma;
--- server/src/server.ts
+++ server/src/server.ts
@@ -73,36 +73,6 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(compression());
-prisma.$use(async (params, next) => {
-  const { model, action, runInTransaction, args } = params;
-  const description = [model, action].filter(Boolean).join('.');
-  const data = {
-    model,
-    action,
-    runInTransaction,
-    args,
-  };
-
-  const scope = getCurrentHub().getScope();
-  const parentSpan = scope?.getSpan();
-  const span = parentSpan?.startChild({
-    op: 'db',
-    description,
-    data,
-  });
-
-  // optional but nice
-  scope?.addBreadcrumb({
-    category: 'db',
-    message: description,
-    data,
-  });
-
-  const result = await next(params);
-  span?.finish();
-
-  return result;
-});
 app.use(cors());
 const getEnveloped = envelop({
   plugins: [
