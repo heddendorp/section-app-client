5c6a88d2d04d776f6eeb6355e3c2593e9926b6a7
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-03-31 15:03:34 +0200

    fix(legacy-app): add icon toast component

--- legacy-app/src/app/modules/shared/components/icon-toast/icon-toast.component.ts
+++ legacy-app/src/app/modules/shared/components/icon-toast/icon-toast.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
+import {
+  MAT_SNACK_BAR_DATA,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-icon-toast',
   templateUrl: './icon-toast.component.html',
   styleUrls: ['./icon-toast.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IconToastComponent implements OnInit {
-  constructor() {}
+export class IconToastComponent {
+  constructor(
+    @Inject(MAT_SNACK_BAR_DATA)
+    public data: {
+      icon?: string;
+      action?: string;
+      message: string;
+      allowClose?: boolean;
+    },
+    private snackbarRef: MatSnackBarRef<IconToastComponent>
+  ) {}
 
-  ngOnInit(): void {}
+  dismiss(): void {
+    this.snackbarRef.dismissWithAction();
+  }
+
+  cancel(): void {
+    this.snackbarRef.dismiss();
+  }
 }
