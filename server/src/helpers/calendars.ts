f079bd227de07d749c826b9a24b675f8a0ac1518
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-13 14:07:51 +0200

    fix(server): improve public events

--- server/src/helpers/calendars.ts
+++ server/src/helpers/calendars.ts
@@ -10,16 +10,38 @@ export const calendarRouter = () => {
     const events = await prisma.tumiEvent.findMany({
       where: { publicationState: PublicationState.PUBLIC },
     });
-    const calendar = ical({ name: 'TUMi public events', ttl: 60 * 60 * 2 });
-    events.forEach((event) =>
+    const calendar = ical({
+      name: 'TUMi public events',
+      ttl: 60 * 60 * 2,
+      method: ICalCalendarMethod.PUBLISH,
+      prodId: {
+        company: 'TUMi e.V.',
+        product: 'All Events',
+      },
+    });
+    events.forEach((event) => {
+      let addon = {};
+      if (
+        typeof event.coordinates === 'object' &&
+        !Array.isArray(event.coordinates) &&
+        event.coordinates?.lat
+      ) {
+        addon = {
+          location: {
+            title: event.location,
+            geo: { lat: event.coordinates?.lat, lon: event.coordinates?.lon },
+          },
+        };
+      }
       calendar.createEvent({
         start: event.start,
         end: event.end,
         summary: event.title,
         description: `More info at: https://tumi.esn.world/events/${event.id}`,
         url: `https://tumi.esn.world/events/${event.id}`,
-      })
-    );
+        ...addon,
+      });
+    });
     calendar.serve(res);
   });
   router.get('/private/:token', async (req, res) => {
