b5be9b1d53fc63aaa6e653a4ba69160e84879f51
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-25 13:19:41 +0000

    Add secrets to dev env

--- .space.kts
+++ .space.kts
@@ -11,12 +11,20 @@ job("My project warmup data") {
         // e.g. 'gradlew assemble'
         // If you want Automation only to build indexes,
         // don't specify scriptLocation
-        scriptLocation = "./dev-env-warmup.sh"
+        // scriptLocation = "./dev-env-warmup.sh"
 
         // (Optional) IDE version to use instead of the
         // organization default. You can also use
         // ideVersion = IdeVersion.LatestOfQuality("Release").
         // ideVersion = IdeVersion.Specific("2021.3", "Release")
+        
+        env["DATABASE_URL"] = Secrets("database-url")
+        env["SENDGRID_API_KEY"] = Secrets("sendgrid-key")
+        env["AUTH0_SECRET"] = Secrets("auth0-secret")
+        env["STRIPE_KEY"] = Secrets("stripe-key")
+        env["BLOB_SAS_TOKEN"] = Secrets("azure-storage-token")
+        env["PRODUCT_SAS_TOKEN"] = Secrets("azure-product-storage-token")
+        env["PHOTO_SAS_TOKEN"] = Secrets("photo-storage-token")
     }
 
     // (Optional, recommended)
