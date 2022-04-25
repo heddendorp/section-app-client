09b18d739edd18cb815a237c2edb7cee609e7730
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-25 12:45:34 +0000

    remove more advanced options

--- .space.kts
+++ .space.kts
@@ -11,11 +11,11 @@ job("My project warmup data") {
         // e.g. 'gradlew assemble'
         // If you want Automation only to build indexes,
         // don't specify scriptLocation
-        scriptLocation = "./dev-env-warmup.sh"
+        // scriptLocation = "./dev-env-warmup.sh"
 
         // (Optional) IDE version to use instead of the
         // organization default. You can also use
-        ideVersion = IdeVersion.LatestOfQuality("Release").
+        // ideVersion = IdeVersion.LatestOfQuality("Release").
         // ideVersion = IdeVersion.Specific("2021.3", "Release")
     }
 
