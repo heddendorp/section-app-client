49025d203c39ec5fc2134a52c598149257c94c0b
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-25 13:37:22 +0000

    Add sh script back

--- .space.kts
+++ .space.kts
@@ -11,18 +11,6 @@ job("My project warmup data") {
     // ide is an IDE you want Automation to build indexes for:
     // Ide.Fleet || Ide.Idea || Ide.GoLand
     warmup(ide = Ide.WebStorm) {
-        // (Optional) path to your custom .sh script
-        // that contains additional warmup steps,
-        // e.g. 'gradlew assemble'
-        // If you want Automation only to build indexes,
-        // don't specify scriptLocation
-        // scriptLocation = "./dev-env-warmup.sh"
-
-        // (Optional) IDE version to use instead of the
-        // organization default. You can also use
-        // ideVersion = IdeVersion.LatestOfQuality("Release").
-        // ideVersion = IdeVersion.Specific("2021.3", "Release")
-        
         env["DATABASE_URL"] = Secrets("database-url")
         env["SENDGRID_API_KEY"] = Secrets("sendgrid-key")
         env["AUTH0_SECRET"] = Secrets("auth0-secret")
@@ -30,6 +18,17 @@ job("My project warmup data") {
         env["BLOB_SAS_TOKEN"] = Secrets("azure-storage-token")
         env["PRODUCT_SAS_TOKEN"] = Secrets("azure-product-storage-token")
         env["PHOTO_SAS_TOKEN"] = Secrets("photo-storage-token")
+        // (Optional) path to your custom .sh script
+        // that contains additional warmup steps,
+        // e.g. 'gradlew assemble'
+        // If you want Automation only to build indexes,
+        // don't specify scriptLocation
+        scriptLocation = "./dev-env-warmup.sh"
+
+        // (Optional) IDE version to use instead of the
+        // organization default. You can also use
+        ideVersion = IdeVersion.LatestOfQuality("Release").
+        // ideVersion = IdeVersion.Specific("2021.3", "Release")
     }
 
     // (Optional, recommended)
