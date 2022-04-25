0631f5517d55e76e7116bfc883f8b732831abc05
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-25 12:43:04 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,29 @@
+job("My project warmup data") {
+    startOn {
+        schedule { cron("0 7 * * *") }
+    }
+
+    // ide is an IDE you want Automation to build indexes for:
+    // Ide.Fleet || Ide.Idea || Ide.GoLand
+    warmup(ide = Ide.WebStorm) {
+        // (Optional) path to your custom .sh script
+        // that contains additional warmup steps,
+        // e.g. 'gradlew assemble'
+        // If you want Automation only to build indexes,
+        // don't specify scriptLocation
+        scriptLocation = "./dev-env-warmup.sh"
+
+        // (Optional) IDE version to use instead of the
+        // organization default. You can also use
+        ideVersion = IdeVersion.LatestOfQuality("Release").
+        // ideVersion = IdeVersion.Specific("2021.3", "Release")
+    }
+
+    // (Optional, recommended)
+    git {
+        // fetch the entire commit history
+        depth = UNLIMITED_DEPTH
+        // fetch all branches
+        refSpec = "refs/*:refs/*"
+    }
+}
