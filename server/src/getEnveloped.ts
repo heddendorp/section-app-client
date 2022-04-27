71262162d0d3d956b152bcede236025554fbb4eb
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-27 19:04:15 +0200

    refactor(server): move from helix to yoga

--- server/src/getEnveloped.ts
+++ server/src/getEnveloped.ts
@@ -15,110 +15,107 @@ import { PrismaClient, Tenant, User } from './generated/prisma';
 import * as Sentry from '@sentry/node';
 import { useGraphQLMiddleware } from '@envelop/graphql-middleware';
 import { permissions } from './permissions';
+import { useResponseCache } from '@envelop/response-cache';
+import { useGraphQlJit } from '@envelop/graphql-jit';
 
 const isProd = process.env.NODE_ENV === 'production';
-export const getEnveloped = envelop({
-  plugins: [
-    useSchema(schema),
-    // useLogger(),
-    // enableIf(!isProd, useTiming()),
-    enableIf(
-      isProd,
-      useHive({
-        enabled: true,
-        debug: process.env.NODE_ENV !== 'production', // or false
-        token: process.env.HIVE_TOKEN ?? '',
-        reporting: {
-          // feel free to set dummy values here
-          author: 'Author of the schema version',
-          commit: 'git sha or any identifier',
+export const envelopPlugins = [
+  useSchema(schema),
+  // useLogger(),
+  // enableIf(!isProd, useTiming()),
+  enableIf(
+    isProd,
+    useHive({
+      enabled: true,
+      debug: process.env.NODE_ENV !== 'production', // or false
+      token: process.env.HIVE_TOKEN ?? '',
+      reporting: {
+        // feel free to set dummy values here
+        author: 'Author of the schema version',
+        commit: 'git sha or any identifier',
+      },
+      usage: {
+        clientInfo(context: any) {
+          const name = context.req.headers['x-graphql-client-name'];
+          const version = context.req.headers['x-graphql-client-version'];
+          if (name && version) {
+            return {
+              name,
+              version,
+            };
+          }
+          return null;
         },
-        usage: {
-          clientInfo(context: any) {
-            const name = context.req.headers['x-graphql-client-name'];
-            const version = context.req.headers['x-graphql-client-version'];
-            if (name && version) {
-              return {
-                name,
-                version,
-              };
-            }
-            return null;
-          },
+      },
+    })
+  ),
+  enableIf(isProd, useSentry()),
+  useAuth0({
+    domain: 'tumi.eu.auth0.com',
+    audience: 'esn.events',
+    extendContextField: 'token',
+  }),
+  useParserCache(),
+  useValidationCache(),
+  useExtendContext(async (context) => {
+    return {
+      tenant: await context.prisma.tenant.findFirst(),
+    };
+  }),
+  useExtendContext(async (context: { token: any; prisma: PrismaClient }) => {
+    if (context.token) {
+      let user = await context.prisma.user.findUnique({
+        where: {
+          authId: context.token.sub,
         },
-      })
-    ),
-    enableIf(isProd, useSentry()),
-    useAuth0({
-      domain: 'tumi.eu.auth0.com',
-      audience: 'esn.events',
-      extendContextField: 'token',
-    }),
-    useParserCache(),
-    useValidationCache(),
-    useExtendContext(async (context) => {
-      return {
-        tenant: await context.prisma.tenant.findFirst(),
-      };
-    }),
-    useExtendContext(async (context: { token: any; prisma: PrismaClient }) => {
-      if (context.token) {
-        let user = await context.prisma.user.findUnique({
-          where: {
+      });
+      if (!user) {
+        user = await context.prisma.user.create({
+          data: {
             authId: context.token.sub,
+            email: '',
+            firstName: '',
+            lastName: '',
+            email_verified: false,
+            picture: '',
           },
         });
-        if (!user) {
-          user = await context.prisma.user.create({
+      }
+      Sentry.setUser({ email: user.email, id: user.id });
+      return {
+        user,
+      };
+    }
+  }),
+  useExtendContext(
+    async (context: { user?: User; prisma: PrismaClient; tenant: Tenant }) => {
+      if (context.user) {
+        let assignment = await context.prisma.usersOfTenants.findUnique({
+          where: {
+            userId_tenantId: {
+              userId: context.user.id,
+              tenantId: context.tenant.id,
+            },
+          },
+        });
+        if (!assignment) {
+          assignment = await context.prisma.usersOfTenants.create({
             data: {
-              authId: context.token.sub,
-              email: '',
-              firstName: '',
-              lastName: '',
-              email_verified: false,
-              picture: '',
+              user: { connect: { id: context.user.id } },
+              tenant: { connect: { id: context.tenant.id } },
             },
           });
         }
-        Sentry.setUser({ email: user.email, id: user.id });
         return {
-          user,
+          assignment,
         };
       }
-    }),
-    useExtendContext(
-      async (context: {
-        user?: User;
-        prisma: PrismaClient;
-        tenant: Tenant;
-      }) => {
-        if (context.user) {
-          let assignment = await context.prisma.usersOfTenants.findUnique({
-            where: {
-              userId_tenantId: {
-                userId: context.user.id,
-                tenantId: context.tenant.id,
-              },
-            },
-          });
-          if (!assignment) {
-            assignment = await context.prisma.usersOfTenants.create({
-              data: {
-                user: { connect: { id: context.user.id } },
-                tenant: { connect: { id: context.tenant.id } },
-              },
-            });
-          }
-          return {
-            assignment,
-          };
-        }
-      }
-    ),
-    // useResponseCache({
-    //   ttl: 2000,
-    //   session: (context) => String(context.user?.id),
-    // }),
-    useGraphQLMiddleware([permissions]),
-  ],
-});
+    }
+  ),
+  useGraphQlJit(),
+  // useResponseCache({
+  //   ttl: 2000,
+  //   session: (context) => String(context.user?.id),
+  // }),
+  useGraphQLMiddleware([permissions]),
+];
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,6 @@
 import express from 'express';
-import {
-  getGraphQLParameters,
-  processRequest,
-  renderGraphiQL,
-  sendResult,
-  shouldRenderGraphiQL,
-} from 'graphql-helix';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
-import cors from 'cors';
 import compression from 'compression';
 import { socialRouter } from './helpers/socialImage';
 import { webhookRouter } from './helpers/webhooks';
@@ -18,7 +10,9 @@ import { shortRouter } from './helpers/shortRouter';
 import prisma from './client';
 import { Auth0 } from './helpers/auth0';
 import { $settings } from './generated/nexus-prisma';
-import { getEnveloped } from './getEnveloped';
+import { envelopPlugins } from './getEnveloped';
+import { createServer } from '@graphql-yoga/node';
+import { schema } from './schema';
 
 declare global {
   namespace Express {
@@ -45,7 +39,16 @@ declare global {
   }
 }
 
-export const app = express();
+const app = express();
+
+const graphQLServer = createServer({
+  schema,
+  context: {
+    prisma,
+    auth0: new Auth0(),
+  },
+  plugins: envelopPlugins,
+});
 
 Sentry.init({
   dsn: 'https://c8db9c4c39354afba335461b01c35418@o541164.ingest.sentry.io/6188953',
@@ -73,7 +76,6 @@ $settings({
 });
 
 app.use(compression());
-app.use(cors());
 app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
@@ -82,49 +84,7 @@ app.use(express.json());
 app.use('/cal', calendarRouter());
 app.use('/qr', qrRouter());
 app.use('/go', shortRouter());
-app.use('/graphql', async (req, res) => {
-  const { parse, validate, contextFactory, execute, schema } = getEnveloped({
-    req,
-    prisma,
-    auth0: new Auth0(),
-  });
-  // Create a generic Request object that can be consumed by Graphql Helix's API
-  const request = {
-    body: req.body,
-    headers: req.headers,
-    method: req.method,
-    query: req.query,
-  };
-
-  // Determine whether we should render GraphiQL instead of returning an API response
-  if (shouldRenderGraphiQL(request)) {
-    res.send(renderGraphiQL());
-  } else {
-    // Extract the Graphql parameters from the request
-    const { operationName, query, variables } = getGraphQLParameters(request);
-
-    // Validate and execute the query
-    const result = await processRequest({
-      operationName,
-      query,
-      variables,
-      request,
-      schema,
-      parse,
-      validate,
-      execute,
-      contextFactory,
-    });
-
-    // processRequest returns one of three types of results depending on how the server should respond
-    // 1) RESPONSE: a regular JSON payload
-    // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
-    // 3) PUSH: a stream of events to push back down the client for a subscription
-    // The "sendResult" is a NodeJS-only shortcut for handling all possible types of Graphql responses,
-    // See "Advanced Usage" below for more details and customizations available on that layer.
-    await sendResult(result, res);
-  }
-});
+app.use('/graphql', graphQLServer);
 app.use(socialRouter);
 app.use(Sentry.Handlers.errorHandler());
 const port = process.env.PORT || 3333;
@@ -132,7 +92,7 @@ const port = process.env.PORT || 3333;
 process.env.NODE_ENV !== 'test' &&
   app.listen(port, () => {
     /*prismaUtils().then(() => {
-    console.log(`DB actions finished`);
-  });*/
+  console.log(`DB actions finished`);
+});*/
     console.log(`GraphQL server is running on port ${port}.`);
   });
