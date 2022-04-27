68b5bc21e65ebf31cfb94c9348edbd4fcb3c58c1
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-04-27 15:51:35 +0200

    fix(legacy-app): improve login backoff

--- legacy-app/src/app/components/auth-button/auth-button.component.ts
+++ legacy-app/src/app/components/auth-button/auth-button.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from '@auth0/auth0-angular';
 import { filter, retry, tap } from 'rxjs';
 import { GetCurrentUserGQL } from '@tumi/legacy-app/generated/generated';
 import { Router } from '@angular/router';
+import { retryBackoff } from 'backoff-rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-auth-button',
@@ -14,20 +16,32 @@ export class AuthButtonComponent {
   constructor(
     @Inject(DOCUMENT) public document: Document,
     public auth: AuthService,
+    private snackBar: MatSnackBar,
     router: Router,
     getUser: GetCurrentUserGQL
   ) {
     auth.isAuthenticated$.pipe(filter((auth) => auth)).subscribe(() => {
       getUser
         .fetch()
-        .pipe(retry(3))
-        .subscribe((user) => {
-          if (
-            !user.data.currentUser ||
-            !user.data.currentUser.profileComplete
-          ) {
-            router.navigate(['/', 'profile', 'new']);
-          }
+        .pipe(retryBackoff({ initialInterval: 100, maxRetries: 5 }))
+        .subscribe({
+          next: (user) => {
+            if (
+              !user.data.currentUser ||
+              !user.data.currentUser.profileComplete
+            ) {
+              router.navigate(['/', 'profile', 'new']);
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.snackBar
+              .open('Error loading user', 'Reload Page', { duration: 0 })
+              .afterDismissed()
+              .subscribe(() => {
+                window.location.reload();
+              });
+          },
         });
     });
   }
