c8fe418794d2057136bc6b7bf64544fc6aa27eff
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-05-09 15:45:46 +0200

    feat(server): install pothos

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -5,7 +5,23 @@ import {
   Decimal,
   Json,
 } from 'nexus-prisma/dist-cjs/entrypoints/scalars';
+// import SchemaBuilder from '@pothos/core';
 // import { Json, Decimal, DateTime } from 'nexus-prisma/';
+
+// const builder = new SchemaBuilder({});
+//
+// builder.queryType({
+//   fields: (t) => ({
+//     hello: t.string({
+//       args: {
+//         name: t.arg.string(),
+//       },
+//       resolve: (parent, { name }) => `hello, ${name || 'World'}`,
+//     }),
+//   }),
+// });
+//
+// export const schema = builder.toSchema({});
 
 export const schema = makeSchema({
   types: [Json, Decimal, DateTime, types],
