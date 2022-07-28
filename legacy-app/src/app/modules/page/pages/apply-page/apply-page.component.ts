6effc83ffff8c5321166529ffdf54d338ac0a6c3
Jacob Zhang <jacob-z@live.de>
2022-07-28 14:50:20 +0200

    Recruiting Page #288

--- legacy-app/src/app/modules/page/pages/apply-page/apply-page.component.ts
+++ legacy-app/src/app/modules/page/pages/apply-page/apply-page.component.ts
@@ -8,6 +8,6 @@ import { Title } from '@angular/platform-browser';
 })
 export class ApplyPageComponent {
   constructor(private title: Title) {
-    this.title.setTitle('TUMi - apply');
+    this.title.setTitle('TUMi - Apply');
   }
 }
