f04662831b6d3d66caf0afb1b739723c3941f926
Jacob Zhang <jacob-z@live.de>
2022-08-02 12:26:27 +0200

    Not subsized option for expenses #231 (#298)
    * Not subsized option for expenses #231
    
    * Format code with prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ legacy-app/src/app/modules/event-templates/components/finance-planner/finance-planner-price-model/finance-planner-price-model.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { PriceModel } from '../finance-planner.component';
+
+@Component({
+  selector: 'app-finance-planner-price-model',
+  templateUrl: './finance-planner-price-model.component.html',
+  styleUrls: ['./finance-planner-price-model.component.scss'],
+})
+export class FinancePlannerPriceModelComponent {
+  @Input() name: string = '';
+  @Input()
+  result!: PriceModel;
+}
