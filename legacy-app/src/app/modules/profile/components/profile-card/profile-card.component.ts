2c16d69308d8b68876102320e3bf01cbafbe855b
Jacob Zhang <jacob-z@live.de>
2022-08-05 03:44:06 +0200

    Highlight tutor status in profile

--- legacy-app/src/app/modules/profile/components/profile-card/profile-card.component.ts
+++ legacy-app/src/app/modules/profile/components/profile-card/profile-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Role, UserProfilePublicQuery, UserProfileQuery } from '@tumi/legacy-app/generated/generated';
+import { MembershipStatus, Role, UserProfilePublicQuery, UserProfileQuery } from '@tumi/legacy-app/generated/generated';
 
 @Component({
   selector: 'app-profile-card',
@@ -17,4 +17,15 @@ export class ProfileCardComponent {
   public edit = new EventEmitter<void>();
 
   public Role = Role;
+
+  position(position: string | null | undefined, status: MembershipStatus | undefined) {
+    if (!!position || !status) return position;
+    if (status === MembershipStatus.Trial) {
+      return 'TUMi Tutor (Trial Member)'
+    }
+    if (status === MembershipStatus.Full) {
+      return 'TUMi Tutor'
+    }
+    return null;
+  }
 }
