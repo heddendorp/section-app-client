c58c8aae19a6afd401fc0825b26191537a65bdad
Jacob Zhang <jacob-z@live.de>
2022-08-09 02:14:34 +0200

    Show exact number of events on apply page

--- legacy-app/src/app/modules/page/pages/apply-page/apply-page.component.ts
+++ legacy-app/src/app/modules/page/pages/apply-page/apply-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import {
+  GetTutorHubEventsGQL,
+  GetTutorHubEventsQuery,
+} from '@tumi/legacy-app/generated/generated';
+import { DateTime } from 'luxon';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-apply-page',
@@ -7,7 +13,25 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./apply-page.component.scss'],
 })
 export class ApplyPageComponent {
-  constructor(private title: Title) {
+  private getTutorHubEventsRef;
+  public events$: Observable<
+    GetTutorHubEventsQuery['currentTenant']['tutorHubEvents']
+  >;
+
+  constructor(
+    private title: Title,
+
+    private getTutorHubEvents: GetTutorHubEventsGQL
+  ) {
     this.title.setTitle('TUMi - Apply');
+    this.getTutorHubEventsRef = this.getTutorHubEvents.watch({
+      range: {
+        start: DateTime.fromObject({ year: 2022, month: 4, day: 1 }),
+        end: DateTime.fromObject({ year: 2022, month: 10, day: 1 }),
+      },
+    });
+    this.events$ = this.getTutorHubEventsRef.valueChanges.pipe(
+      map(({ data }) => data.currentTenant.tutorHubEvents)
+    );
   }
 }
