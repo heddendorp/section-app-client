9c854e5ee494b6c1c9c8d58a0a82ece735295b7d
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-09-21 14:37:13 +0200

    feat(legacy-app): add overview for eventsubmissions (#429)

---
+++ legacy-app/src/app/modules/events/components/event-submission-overview/event-submission-overview.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventSubmissionOverviewComponent } from './event-submission-overview.component';
+
+describe('EventSubmissionOverviewComponent', () => {
+  let component: EventSubmissionOverviewComponent;
+  let fixture: ComponentFixture<EventSubmissionOverviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EventSubmissionOverviewComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EventSubmissionOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
