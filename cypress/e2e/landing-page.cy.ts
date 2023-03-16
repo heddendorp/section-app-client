9b70319f94eaeadd8427993af9f5476456db20cb
Lukas Heddendorp <lu.heddendorp@gmail.com>
2023-03-16 10:38:05 +0100

    (chore): remove cypress tests

--- cypress/e2e/landing-page.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('The Landing Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/');
-  });
-  it('redirects to events', () => {
-    cy.visit('/');
-    cy.url().should('include', '/events');
-  });
-});
