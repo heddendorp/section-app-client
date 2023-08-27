70db61e40b0e671b641527f87959aa1033827d13
essare-rimaz <71963709+essare-rimaz@users.noreply.github.com>
2023-08-27 22:42:55 +0200

    Update cu-prague homepage (#832)

--- src/app/modules/home/components/cu-prague-home/cu-prague-home.component.ts
+++ src/app/modules/home/components/cu-prague-home/cu-prague-home.component.ts
@@ -1,5 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {
+  GetHomePageDataGQL,
+  GetHomePageDataQuery,
+} from '@tumi/legacy-app/generated/generated';
+import { Title } from '@angular/platform-browser';
+import { IconURLPipe } from '@tumi/legacy-app/modules/shared/pipes/icon-url.pipe';
+import { ExtendDatePipe } from '@tumi/legacy-app/modules/shared/pipes/extended-date.pipe';
 import { TechnicalSupportComponent } from '../technical-support/technical-support.component';
+import { GridComponent } from '../../../shared/components/grid/grid.component';
+import { NgFor, AsyncPipe, DatePipe, NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cu-prague-home',
@@ -7,6 +20,35 @@ import { TechnicalSupportComponent } from '../technical-support/technical-suppor
   styleUrls: ['./cu-prague-home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [TechnicalSupportComponent],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    MatIconModule,
+    NgFor,
+    GridComponent,
+    TechnicalSupportComponent,
+    AsyncPipe,
+    DatePipe,
+    ExtendDatePipe,
+    IconURLPipe,
+    NgOptimizedImage,
+  ],
 })
-export class CuPragueHomeComponent {}
+export class CuPragueHomeComponent {
+  public events$: Observable<GetHomePageDataQuery['events']>;
+  public loggedIn$: Observable<boolean>;
+
+  constructor(
+    private q: GetHomePageDataGQL,
+    private title: Title,
+  ) {
+    this.title.setTitle('Home - CU Prague');
+
+    this.events$ = this.q
+      .watch()
+      .valueChanges.pipe(map(({ data }) => data.events));
+    this.loggedIn$ = this.q
+      .watch()
+      .valueChanges.pipe(map(({ data }) => !!data.currentUser?.id));
+  }
+}
