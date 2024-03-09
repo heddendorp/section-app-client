29da9dc290bdb6b86a0a9ded6d0612feeb954743
Youssef Magdy <91832228+youssefmagdyy@users.noreply.github.com>
2024-03-09 20:31:40 +0100

    VUB-EhB Homepage (#844)

--- src/app/modules/home/components/vub-ehb-home/vub-ehb-home.component.ts
+++ src/app/modules/home/components/vub-ehb-home/vub-ehb-home.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TechnicalSupportComponent } from '../technical-support/technical-support.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vub-ehb-home',
@@ -7,6 +8,6 @@ import { TechnicalSupportComponent } from '../technical-support/technical-suppor
   styleUrls: ['./vub-ehb-home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [TechnicalSupportComponent],
+  imports: [TechnicalSupportComponent, CommonModule], 
 })
 export class VubEhbHomeComponent {}
